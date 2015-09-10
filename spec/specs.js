0e630dfa296206d6951cb3f286e1f33f5c666439
Ryan Heupler & Leah Klamkin <student@epicodus.com>
2015-09-10 10:47:44 -0700

    specs passing for all numbers

--- spec/specs.js
+++ spec/specs.js
@@ -1,28 +1,29 @@
 describe("roman", function() {
   it("pushes M into the array if the number is 1000 or higher", function() {
-    expect(roman(2000)).to.eql(["M", "M"]);
+    expect(roman(2000)).to.eql("MM");
   });
 
   it("Pushes CM into array for number greater than  or eq 900, less than 1000", function() {
-    expect(roman(900)).to.eql(["CM"]);
+    expect(roman(900)).to.eql("CM");
   });
 
   it("Pushes D into array for number greater than  or eq 500", function() {
-    expect(roman(500)).to.eql(["D"]);
+    expect(roman(500)).to.eql("D");
   });
 
   it("Pushes C into array for number greater than  or eq 100", function() {
-    expect(roman(100)).to.eql(["C"]);
+    expect(roman(100)).to.eql("C");
   });
 
   it("Pushes X into array for number greater than  or eq 90", function() {
-    expect(roman(90)).to.eql(["X"]);
+    expect(roman(90)).to.eql("XC");
   });
 
   it("Pushes L into array for number greater than  or eq 50", function() {
-    expect(roman(50)).to.eql(["L"]);
+    expect(roman(50)).to.eql("L");
   });
 
-  it("Pushes X into array for number greater than  or eq 50", function() {
-    expect(roman(10)).to.eql(["X"]);
+  it("Pushes X into array for number greater than  or eq 10", function() {
+    expect(roman(10)).to.eql("X");
+});
 });
