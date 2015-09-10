c538f70b555b15c3e8e4279f4f5fe4a8cf71f845
Ryan Heupler & Leah Klamkin <student@epicodus.com>
2015-09-10 10:28:05 -0700

    working specs for 900, 500, 100

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,20 @@ var roman = function(number) {
     numerals.push("M");
     number -= 1000;
   };
+
+  while (number >= 900) {
+    numerals.push("CM");
+    number -= 900;
+  };
+
+  while (number >= 500) {
+    numerals.push("D");
+    number -= 500;
+  };
+
+  while (number >= 100) {
+    numerals.push("C");
+    number -= 100;
+  };
   return numerals;
 };
--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,16 @@ describe("roman", function() {
   it("pushes M into the array if the number is 1000 or higher", function() {
     expect(roman(2000)).to.eql(["M", "M"]);
   });
+
+  it("Pushes C into array for number greater than  or eq 900, less than 1000", function() {
+    expect(roman(900)).to.eql(["CM"]);
+  });
+
+  it("Pushes D into array for number greater than  or eq 500", function() {
+    expect(roman(500)).to.eql(["D"]);
+  });
+
+  it("Pushes D into array for number greater than  or eq 500", function() {
+    expect(roman(100)).to.eql(["C"]);
+  });
 });
